a731cdb7e2e60a80be3b9068e3dbf141901d050a
= <bhagwatkhandekar312@gmail.com>
2021-11-16 13:32:52 +0530

    Dummy id's removed

--- app/controllers/all_players_controller.rb
+++ app/controllers/all_players_controller.rb
@@ -2,7 +2,6 @@ class AllPlayersController < ApplicationController
 
   def index
      user_ids = User.joins(:players).group('users.id').having("count(players.id)>=10")
-    user_ids  = User.ids
     @users = User.where(id: user_ids).includes(:players)
   end
 end
